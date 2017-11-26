f8b8a05fce12976384034eb11e4452522b4604e9
Oliver <subscriptions@littlemonsterdigital.co.uk>
2017-11-26 20:07:33 +0000

    v1.0.2
    - Added dynamic tables to display league standings
    - added template-league.php
    - added section-league-descrtiption.php
    - added section-league-events.php
    - added section-league standings.php
    - Removed individual league sections

---
+++ template-parts/section-league-description.php
@@ -0,0 +1,5 @@
+<div class="" style="width:100%;text-align:center;">
+    <h2 style="color:#c3f216;margin:0;"><strong>KINGS</strong>  <?php echo get_the_title(); ?> League</h2>
+    <p class="lead">Information &amp; standings for clubs affiliated to the <?php echo get_the_title(); ?> League</p>
+    <section class="league-description" style="text-align:left;"><?php the_field('league_description'); ?></section>
+</div>
